42d45c09369bf8b5c63c31c60e8dba387211fed7
Bruno Belarte <bruno.belarte@thoughtworks.com>
2023-04-11 12:11:49 +0200

    Add connections counter

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-socket/pkg/echoing"
 	"log"
+	"sync/atomic"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -11,19 +12,25 @@ import (
 	"nhooyr.io/websocket"
 )
 
+var connections uint64
+var disconnections uint64
+
 func hello(ctx echo.Context) error {
 	c, err := websocket.Accept(ctx.Response().Writer, ctx.Request(), nil)
-	log.Print("Connecting")
 	if err != nil {
 		return err
 	}
 	defer c.Close(websocket.StatusInternalError, "the sky is falling")
 
+	atomic.AddUint64(&connections, 1)
+	log.Print("Connections: ", connections)
+
 	l := rate.NewLimiter(rate.Every(time.Millisecond*100), 10)
 	for {
 		err = echoing.Echo(ctx.Request().Context(), c, l)
 		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
-			log.Print("Disconnecting...")
+			atomic.AddUint64(&disconnections, 1)
+			log.Printf("Status: connections=%d, disconnections=%d", connections-disconnections, disconnections)
 			return nil
 		}
 		if err != nil {
